0686a25a0d42f807cda947eeb06b5e5623355110
ShotaArima <g2172010@tcu.ac.jp>
2025-07-02 18:03:19 +0900

    reflesh css

--- my-resume/src/pages/Top.tsx
+++ my-resume/src/pages/Top.tsx
@@ -3,10 +3,14 @@ import styles from './Top.module.css';
 
 const Top: React.FC = () => {
   return (
-    <section
-      id="top"
-      className={`relative min-h-screen flex items-center justify-center text-center overflow-hidden ${styles.heroSection}`}
-    >
+    <section id="top" className={`relative min-h-screen flex items-center justify-center text-center overflow-hidden`}>
+      
+      {/* 画像：幅に合わせる */}
+      <img
+        src={`${process.env.PUBLIC_URL}/background-image.jpeg`}
+        alt="背景画像"
+        className={`${styles.heroImage}`}
+      />
 
       {/* 内容：前面に出す */}
       <div className="relative z-10 p-6 text-white">
