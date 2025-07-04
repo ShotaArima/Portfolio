c15a66098876bc3240eb562e28b03aae01bd6506
ShotaArima <g2172010@tcu.ac.jp>
2025-07-05 01:22:40 +0900

    remove Link from App.tsx import

--- my-resume/src/App.tsx
+++ my-resume/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Top from './pages/Top';
 import Career from './pages/Career';
 import Skills from './pages/Skills';
