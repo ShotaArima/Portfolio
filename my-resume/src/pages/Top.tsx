a06a1d71c051d091e0bb18c97b582a8041edfc8f
ShotaArima <g2172010@tcu.ac.jp>
2025-07-04 23:36:47 +0900

    recreate Top

--- my-resume/src/pages/Top.tsx
+++ my-resume/src/pages/Top.tsx
@@ -1,16 +1,22 @@
 import React, { useEffect, useState } from 'react';
-  
+import ReactMarkdown from 'react-markdown';
+
 const Top: React.FC = () => {
+  const [markdown, setMarkdown] = useState<string>('');
+  
+  useEffect(() => {
+    fetch(`${process.env.PUBLIC_URL}/markdowns/top.md`)
+      .then((res) => res.text())
+      .then((text) => setMarkdown(text));
+  }, []);
+
   return (
-    <section id="Top">
-      <h2>スキル</h2>
-      <ul>
-        <li>JavaScript, TypeScript</li>
-        <li>React, Node.js</li>
-        <li>Python, Django</li>
-        <li>Git, Docker</li>
-        <li>資格: AWS認定ソリューションアーキテクト</li>
-      </ul>
+    <section id="Top" className="text-left w-full p-4">
+      <h1>Shota Arima</h1>
+      <p>(Handle name: 学生室の住人)</p>
+      
+      <ReactMarkdown>{markdown}</ReactMarkdown>
+      
     </section>
   );
 };
